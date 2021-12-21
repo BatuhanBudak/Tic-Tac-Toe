1895ad2246fad28e9d0eb069480d777d4d2d836c
Batuhan <batuhan.budak@gmail.com>
2021-12-21 20:00:07 +0300

    Added round win and game win logic

--- script.js
+++ script.js
@@ -5,29 +5,49 @@ let gameBoardArray = [];
 const createGameBoard = function(){
     
     
-    const boardLength = 9; //Fixed game board 
+    const boardRowAndColumnLength = 3; //Fixed game board 
     const gameBoardContainer = document.querySelector(".game-board-container");
     gameBoardContainer.style.display = "flex";
     
-   
+    function matrix( rows, cols, arr){
+        
+        // Creates all lines:
+        for(var i=0; i < rows; i++){
+      
+            // Creates an empty line
+            arr.push([]);
+      
+            // Adds cols to the empty line:
+            arr[i].push( new Array(cols));
+        }
+      
+      return arr;
+      }
+      
 
     function _populateGameBoardArray() {
 
-       for (let index = 0; index < boardLength; index++) {
-        gameBoardArray.push(GameBoardNode(index));}
-    }
+        gameBoardArray =  matrix(3, 3, gameBoardArray)
+
+       for (let i = 0; i < boardRowAndColumnLength; i++) {
+           for (let j = 0; j < boardRowAndColumnLength; j++) {
+            gameBoardArray[i][j] = GameBoardNode(`${i}${j}`);
+        
+           }
+        }}
   
     function _populateGameBoardContainer ()  {
-        for (let i = 0; i < gameBoardArray.length; i++) {
+        for (let i = 0; i < boardRowAndColumnLength; i++) {
+            for (let j = 0; j < boardRowAndColumnLength; j++) {
 
             let boardNode = document.createElement("div");
             boardNode.classList.add("board-node");
             boardNode.textContent = "";
-            boardNode.id = gameBoardArray[i].getId();
+            boardNode.id = gameBoardArray[i][j].getId();
             gameBoardContainer.appendChild(boardNode);
+            }
         }
     }
-    
     
     _populateGameBoardArray();
     _populateGameBoardContainer();
@@ -42,11 +62,13 @@ const GameBoardNode = function Node(id, value = "") {
     const getId = () => id;
     const getValue = () => value;
     const getMark = () => isMarked;
+    const resetMark = () => isMarked = false;
+    const resetValue = () => value = "";
     const changeValue = x => {
         value = x;
         isMarked = true;}
     
-    return {getId, getValue, getMark, changeValue};
+    return {getId, getValue, getMark, changeValue, resetMark, resetValue};
 };
 
 const Player = (name, signature) => {
@@ -60,7 +82,7 @@ const Player = (name, signature) => {
         isPlayed = !getPlayStatus();
     }
     const getScore = () => score;
-    const incrementPlayerScore = () => ++this.score;
+    const incrementPlayerScore = () => ++score;
     return {getSignature, getPlayStatus, changePlayStatus, getScore, incrementPlayerScore, getName};
 }
 
@@ -87,37 +109,132 @@ function handleSubmit(e){
 
 const gameController = () => {
 
-    let currentPlayerName = playerOne.getName();
+    let moveCount = 0;
+    let roundCount = 0;
+    const totalRowsAndColumns = 3;
     let currentPlayer = playerOne;
+
     const boardNodes = Array.from(document.querySelectorAll(".board-node"));
     boardNodes.forEach(node => node.addEventListener("click", changeNodeValue));
-       
-    
-   /*while (true){
-        if(currentPlayer.name === playerOne.getName() && !currentPlayer.getPlayStatus()){
-
-        }
-
-        else if(currentPlayer=== playerTwo.getName() && !currentPlayer.getPlayStatus()){
-
-        }
-    }*/
+  
     function changeNodeValue(e) {
 
-        let targetGameNode = gameBoardArray[e.target.id];
-        if(targetGameNode.getMark()) return;
+        let targetGameNode = gameBoardArray[(e.target.id)[0]][(e.target.id)[1]];
+        if ( targetGameNode.getMark()) return;
+
         targetGameNode.changeValue(currentPlayer.getSignature());
         e.target.textContent = currentPlayer.getSignature();
-        currentPlayer.changePlayStatus();
+
+        currentPlayer.changePlayStatus(); //Doesnt do anything
+        
         if( currentPlayer.getName() == playerOne.getName()) {
             currentPlayer = playerTwo;}
         else {
             currentPlayer = playerOne;
         }
-        //render()
-        
+
+        moveCount++;
+        checkRoundEndConditions(targetGameNode.getValue(),(e.target.id)[0],(e.target.id)[1]);
+        checkGameEndConditions(playerOne, playerTwo);
     }
+
+    function checkRoundEndConditions(nodeValue, currentRow, currentColumn ){
+        //check col
+        for(let i = 0; i < totalRowsAndColumns; i++){
+            if(gameBoardArray[currentRow][i].getValue() != nodeValue)
+            {
+                
+                break;}
+            if(i == totalRowsAndColumns-1){
+                console.log(currentPlayer.getName() + "has won!")
+                currentPlayer.incrementPlayerScore();
+                roundCount++;
+                clearBoard();
+            }
+        }
+        //check row
+        for(let i = 0; i < totalRowsAndColumns; i++){
+            if(gameBoardArray[i][currentColumn].getValue() != nodeValue)
+            {
+                
+                break;}
+            if(i == totalRowsAndColumns-1){
+                console.log(currentPlayer.getName() + "has won!")
+                currentPlayer.incrementPlayerScore();
+                roundCount++;
+                clearBoard();
+            }
+        }
+        //check diag
+        if(currentRow == currentColumn){
+            //we're on a diagonal
+            for(let i = 0; i < totalRowsAndColumns; i++){
+                if(gameBoardArray[i][i].getValue() != nodeValue){
+                    break;
+                }
+                if(i == totalRowsAndColumns-1){
+                    console.log(currentPlayer.getName() + "has won!")
+                    currentPlayer.incrementPlayerScore();
+                    roundCount++;
+                    clearBoard();
+                }
+            }
+        }
+         //check anti diag
+         if(currentRow + currentColumn == totalRowsAndColumns - 1){
+            for(let i = 0; i < totalRowsAndColumns; i++){
+                if(gameBoardArray[i][(totalRowsAndColumns-1)-i].getValue() != nodeValue)
+                {
+                    break;
+                }
+                if(i == totalRowsAndColumns-1){
+                    console.log(currentPlayer.getName() + "has won!")
+                    currentPlayer.incrementPlayerScore();
+                    roundCount++;
+                    clearBoard();
+                }
+            }
+        }
+        //check draw
+        if(moveCount == (Math.pow(totalRowsAndColumns, 2) - 1)){
+            console.log("It's a draw.")
+            roundCount++;
+            clearBoard();
+
+        }
+    }
+    function checkGameEndConditions(firstPlayer, secondPlayer){
+        switch(true) {
+            case (firstPlayer.getScore() == 3 && secondPlayer.getScore()== 0 ):
+                console.log("Game over." + `${firstPlayer.getName()} has won!`)
+                break;
+            case ( secondPlayer.getScore() == 3 && firstPlayer.getScore()== 0 ):
+                console.log("Game over." + `${secondPlayer.getName()} has won!`)
+                break;
+            case ( roundCount >= 5 ):
+                firstPlayer.getScore() > secondPlayer.getScore() ? 
+                console.log("Game over." + `${firstPlayer.getName()} has won!`):
+                console.log("Game over." + `${secondPlayer.getName()} has won!`);
+                break;
+            default:
+                break;
+        }
+    }
+    function clearBoard(){
+        gameBoardArray.forEach(nodeGroup => { nodeGroup.forEach(node => {
+            node.resetMark();
+            node.resetValue();
+        })
+        });
+        let boardNodes = Array.from(document.querySelectorAll(".board-node"));
+        boardNodes.forEach(x => x.textContent = "");
+
+        currentPlayer = playerOne;
+        moveCount = 0;
+    }
+
 }
+
 
 
 createPlayers();
